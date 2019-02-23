b582a5b5ff383998824945167266d51fa06ada00
chenliuqing <thliuqing@163.com>
2019-02-23 14:45:37 +0800

    add earth texture

--- DeltaDemo/Assets/Script/PlanetMove.cs
+++ DeltaDemo/Assets/Script/PlanetMove.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class PlanetMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
     void Update()
     {
         
