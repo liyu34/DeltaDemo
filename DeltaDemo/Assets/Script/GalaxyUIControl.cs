08dcf1088c36a1e3d13617d0e5ac806a42f701cc
chenliuqing <thliuqing@163.com>
2019-02-24 01:40:50 +0800

    add Galaxy UI

---
+++ DeltaDemo/Assets/Script/GalaxyUIControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyUIControl : MonoBehaviour
+{
+    public GameObject[] galaxyOnImage;
+
+    public void OnSelectGalaxy()
+    {
+
+    }
+}
