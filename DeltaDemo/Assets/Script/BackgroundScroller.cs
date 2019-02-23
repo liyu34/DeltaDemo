a41b46e4f03ac8779e5595076fa5d36ae51c5b8b
chenliuqing <thliuqing@163.com>
2019-02-23 11:52:19 +0800

    change camera

--- DeltaDemo/Assets/Script/BackgroundScroller.cs
+++ DeltaDemo/Assets/Script/BackgroundScroller.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 地图移动速度
+    public float scrollSpeed = 1;
+    // 一块地图的长度
+    public float widthSize = 40;
+
+    private Vector3 startPosition;
+    
     void Start()
     {
-        
+		startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, widthSize);
+        //transform.position = startPosition + Vector3.forward * newPosition;
     }
 }
